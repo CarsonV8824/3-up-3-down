7df6aca4bf6af648f014b57f33dddc1d60d9ebc8
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-05-02 17:08:17 -0500

    done

---
+++ game/Player.java
@@ -0,0 +1,14 @@
+package game;
+import java.util.ArrayList;
+
+
+public class Player {
+    private ArrayList<Card> handCards;
+    private ArrayList<Card> topCards;
+    private ArrayList<Card> bottomCards;
+
+    public Player() {
+        this.handCards = handCards;
+    }
+
+}
